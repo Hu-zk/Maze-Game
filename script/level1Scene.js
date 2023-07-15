d10a407865230f15eabff87b33a59353951a4c56
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-07-16 00:27:34 +0300

    creating upwards sprite animation

--- script/level1Scene.js
+++ script/level1Scene.js
@@ -38,6 +38,12 @@ class Level1Scene extends Phaser.Scene {
             frameRate:8,
             repeat:-1
         });
+        this.anims.create({
+            key:'up',
+            frames:this.anims.generateFrameNumbers("player",{start:8,end:11}),
+            frameRate:8,
+            repeat:-1
+        });
     
     }
 
