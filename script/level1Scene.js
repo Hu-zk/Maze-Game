876afd47c6e39450364afa91683a6717b60dde11
AhmadEid9 <ahmad.eid.2.4.99@gmail.com>
2023-07-16 14:55:34 +0300

    Finishing the coins scroring in Level1

--- script/level1Scene.js
+++ script/level1Scene.js
@@ -21,6 +21,7 @@ class Level1Scene extends Phaser.Scene {
 
       this.load.image('player', '../assets/Player1.png');
       this.load.image('coin','../assets/coin-box.png')
+      this.load.image('finish','../assets/finish-flags-hi.png')
 
     }
 
@@ -78,6 +79,79 @@ class Level1Scene extends Phaser.Scene {
       // Player movement speed
       const speed = 200;
 
+
+      //Finding the far coins from the path
+      this.pickCoinFar = function(pl, coin){
+        coin.destroy();
+        this.score += 5;
+        console.log('your score is : ' + this.score );
+      }
+
+      //finding the near coins to the path
+      this.pickCoinNear = function(pl, coin){
+        coin.destroy();
+        this.score += 1;
+        console.log('your score is : ' + this.score );
+      }
+
+      this.hitFinish = function (pl, finito) {
+        this.add.text(200, 200, 'Hello World', { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif', Color: '#ff0000' });
+      }
+      
+      //Creating and Positioning the coins
+      this.coin1 = this.physics.add.sprite(80, 350,'coin')
+      this.coin2 = this.physics.add.sprite(700, 500,'coin')
+      this.coin3 = this.physics.add.sprite(80, 90,'coin')
+      this.coin4 = this.physics.add.sprite(540, 350,'coin')
+      this.coin5 = this.physics.add.sprite( 650, 190,'coin')
+      this.coin6 = this.physics.add.sprite(400, 450,'coin')
+      this.coin7 = this.physics.add.sprite(250, 200,'coin')
+      this.coin8 = this.physics.add.sprite(350, 330,'coin')
+
+      //Creating and Positioning the Finish point
+
+      this.finish = this.physics.add.sprite(620, 55,'finish')
+      this.finish.setScale(0.12)
+
+
+
+      //Giving the coins the right size
+      this.coin1.setScale(0.15)
+      this.coin2.setScale(0.15)
+      this.coin3.setScale(0.15)
+      this.coin4.setScale(0.15)
+      this.coin5.setScale(0.15)
+      this.coin6.setScale(0.15)
+      this.coin7.setScale(0.15)
+      this.coin8.setScale(0.15)
+
+      //Set coins to be Immovable
+
+      this.coin1.setImmovable(true)
+      this.coin2.setImmovable(true)
+      this.coin3.setImmovable(true)
+      this.coin4.setImmovable(true)
+      this.coin5.setImmovable(true)
+      this.coin6.setImmovable(true)
+      this.coin7.setImmovable(true)
+      this.coin8.setImmovable(true)
+
+      this.finish.setImmovable(true)
+
+
+      this.score = 0;
+
+      //Assigning the overlap action
+      this.physics.add.overlap(player, this.coin1, this.pickCoinFar, null, this);
+      this.physics.add.overlap(player, this.coin2, this.pickCoinFar, null, this);
+      this.physics.add.overlap(player, this.coin3, this.pickCoinFar, null, this);
+      this.physics.add.overlap(player, this.coin4, this.pickCoinFar, null, this);
+      this.physics.add.overlap(player, this.coin5, this.pickCoinNear, null, this);
+      this.physics.add.overlap(player, this.coin6, this.pickCoinNear, null, this);
+      this.physics.add.overlap(player, this.coin7, this.pickCoinNear, null, this);
+      this.physics.add.overlap(player, this.coin8, this.pickCoinNear, null, this);
+      
+
       this.update = function() {
         // Horizontal movement
         if (cursors.left.isDown) {
@@ -105,24 +179,10 @@ class Level1Scene extends Phaser.Scene {
         player.body.debugShowDirection = false;
     
         // Any logic Your code herethis.coin1 = this.add.sprite(80, 350,'coin')
-        this.coin1 = this.add.sprite(80, 350,'coin')
-      this.coin2 = this.add.sprite(700, 500,'coin')
-      this.coin3 = this.add.sprite(80, 90,'coin')
-      this.coin4 = this.add.sprite(540, 350,'coin')
-      this.coin5 = this.add.sprite(700, 90,'coin')
 
-      this.coin1.setScale(0.15)
-      this.coin2.setScale(0.15)
-      this.coin3.setScale(0.15)
-      this.coin4.setScale(0.15)
-      this.coin5.setScale(0.15)
 
-      this.score = 0;
-      this.physics.add.overlap(player, this.coin1, this.pickCoinNear, null, this);
-      this.physics.add.collider(player,this.coin2);
-      this.physics.add.collider(player,this.coin3);
-      this.physics.add.collider(player,this.coin4);
-      this.physics.add.collider(player,this.coin5);
+      //Creating and possitioning the coins
+      
 
       };
     }
